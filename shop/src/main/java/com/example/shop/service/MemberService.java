04f1503dc8de98a3c68b8a72a3651c7e53987155
ryugahee <gagml910@hanmail.net>
2023-10-25 06:29:13 +0900

    회원가입

---
+++ shop/src/main/java/com/example/shop/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.example.shop.service;
+
+import com.example.shop.entity.Member;
+import com.example.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional  // 에러 발생시 콜백시켜줌
+@RequiredArgsConstructor  // 생성자 생성
+public class MemberService {
+
+    // 생성자가 1개이면 어노테이션 없어도 됨
+    private final MemberRepository memberRepository;
+
+    /*
+     * 회원가입
+     * */
+    public Member saveMember(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    // 중복 회원의 경우 예외 발생시킴
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
